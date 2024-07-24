693b16be0bfcee6dc988f8e1564f236794646ddc
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-07-24 16:16:11 +0200

    Minr fixes in lessons 2 and 3

--- lesson-3/providers/llamacpp_provider.py
+++ lesson-3/providers/llamacpp_provider.py
@@ -9,6 +9,6 @@ class LLamaCppProvider(LLMProvider):
         transformers_path = os.path.expanduser(self.config['transformers_path'])
         self.model = Llama(model_path=transformers_path + "/" + model_path)
 
-    def generate(self, messages):
+    def generate(self, prompt_template, messages):
         result = self.model.create_chat_completion([messages[-1]], max_tokens=None)
         return result['choices'][0]['message']['content']
