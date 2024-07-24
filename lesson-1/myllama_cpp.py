ec26d293035aaab5cc80f20f5d10829c2bdc5d95
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-07-24 15:34:37 +0200

    Fix lesson-1

--- lesson-1/myllama_cpp.py
+++ lesson-1/myllama_cpp.py
@@ -7,10 +7,14 @@ cache_dir = os.getenv('TRANSFORMERS_CACHE', default=os.path.join(os.path.expandu
 # Specify the model path
 model_path = cache_dir + "/llama-2-7b-chat-gguf/llama-2-7b-chat.Q2_K.gguf"
 
-llm = Llama(model_path=model_path)
+llm = Llama(model_path=model_path, chat_format="llama-2")
+
+messages = [
+    {"role": "user", "content": "Who is Robinson Crusoe?"},
+]
 
 # Execute the text generation
-result = llm("Who is Robinson Crusoe?", max_tokens=100)
+result = llm.create_chat_completion(messages)
 
 # Print the result
-print(result['choices'][0]['text'])
+print(result['choices'][0]['message']['content'])
