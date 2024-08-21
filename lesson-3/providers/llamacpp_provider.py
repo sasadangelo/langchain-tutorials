455a11e3b2c94747efabafab3bb551bb16f8913b
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-21 10:39:54 +0200

    Minor fix in llamacpp_provider.py in the lesson 3

--- lesson-3/providers/llamacpp_provider.py
+++ lesson-3/providers/llamacpp_provider.py
@@ -5,9 +5,11 @@ from llama_cpp import Llama
 class LLamaCppProvider(LLMProvider):
     def create_model(self):
         model_path = self.config['model_path']
+        chat_format = self.config['chat_format']
         transformers_path = os.path.expanduser(self.config['transformers_path'])
-        self.model = Llama(model_path=transformers_path + "/" + model_path)
+        self.model = Llama(model_path=transformers_path + "/" + model_path, chat_format=chat_format)
 
-    def generate(self, prompt_template, messages):
+    def generate(self, messages):
+        print(messages[-1])
         result = self.model.create_chat_completion([messages[-1]], max_tokens=None)
         return result['choices'][0]['message']['content']
