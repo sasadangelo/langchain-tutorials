1f84359af4346a5086a57c0147abeba24bf4732b
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-21 12:37:57 +0200

    Minor fix in lesson 3 for WatsonX

--- lesson-3/providers/watsonx_provider.py
+++ lesson-3/providers/watsonx_provider.py
@@ -5,13 +5,13 @@ class WatsonXProvider(LLMProvider):
     def create_model(self):
         self.url = self.config.get("api_url")
         self.project_id = self.config.get("project_id")
-        parameters = self.config.get("parameters", {})
+        self.parameters = self.config.get("parameters", {})
 
         self.model = WatsonxLLM(
             model_id=self.config["model"],
             url=self.url,
             project_id=self.project_id,
-            params=parameters
+            params=self.parameters
         )
 
     def generate(self, messages):
