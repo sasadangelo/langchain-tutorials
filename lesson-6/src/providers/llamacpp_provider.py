8a27a5ea202d9ecd483ebee962a2c46d9ed3bb07
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-21 17:43:15 +0200

    Fix lesson 6

--- lesson-6/src/providers/llamacpp_provider.py
+++ lesson-6/src/providers/llamacpp_provider.py
@@ -1,20 +1,42 @@
 import os
+from typing import List, Union
 from providers.provider import LLMProvider
 from llama_cpp import Llama
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
+import pprint
 
 class LLamaCppProvider(LLMProvider):
     def create_model(self):
         model_path = self.config['model_path']
+        chat_format = self.config['chat_format']
+        self.system_message = self.config['system_message']
         transformers_path = os.path.expanduser(self.config['transformers_path'])
-        self.model = Llama(model_path=transformers_path + "/" + model_path)
+        self.model = Llama(model_path=transformers_path + "/" + model_path, chat_format=chat_format, n_ctx=512)
 
-    def generate(self, prompt):
-        result = self.model(prompt, max_tokens=200)
-        answer = result['choices'][0]['text']
+    def generate(self, messages):
+        format_messages = self.__format_messages(messages)
+        pprint.pprint("****************************************************************")
+        pprint.pprint("Chat History + Question:")
+        pprint.pprint(format_messages)
+        pprint.pprint("****************************************************************")
+        result = self.model.create_chat_completion(format_messages, max_tokens=None)
+        return result['choices'][0]['message']['content']
 
-        # Cleanup the answer
-        answer_cleaned_up = answer.split(': ', 1)[-1]
-        if answer_cleaned_up.startswith('\n'):
-            answer_cleaned_up = answer_cleaned_up[1:]
+    # Depending on message type in input it returns:
+    # - system
+    # - user
+    # - assistant
+    def __get_role(self, message: Union[SystemMessage, HumanMessage, AIMessage]) -> str:
+        if isinstance(message, HumanMessage):
+            return "user"
+        elif isinstance(message, AIMessage):
+            return "assistant"
+        else:
+            return "system"
 
-        return answer_cleaned_up
+    # Convert the prompt messages list format
+    def __format_messages(self, messages: List[Union[SystemMessage, HumanMessage, AIMessage]]) -> List[dict]:
+        formatted_messages = [{"role": "system", "content": self.system_message}]
+        for message in messages:
+            formatted_messages.append({"role": self.__get_role(message), "content": message.content})
+        return formatted_messages
--- lesson-6/src/providers/openai_provider.py
+++ lesson-6/src/providers/openai_provider.py
@@ -1,12 +1,28 @@
 from providers.provider import LLMProvider
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage
 
 class OpenAIProvider(LLMProvider):
     def create_model(self):
         model_name = self.config['model']
         temperature = self.config['temperature']
+        system_message = self.config['system_message']
         self.model = ChatOpenAI(temperature=temperature, model_name=model_name)
+        self.chat_prompt_template = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(content=system_message),
+                MessagesPlaceholder(variable_name="messages"),
+            ]
+        )
 
-    def generate(self, prompt):
-        result = self.model.invoke(prompt)
+    def generate(self, messages):
+        # Generate the prompt with the template
+        formatted_prompt = self.chat_prompt_template.format(messages=messages)
+        print("****************************************************************")
+        print("Chat History + Question:")
+        print(formatted_prompt)
+        print("****************************************************************")
+
+        result = self.model.invoke(formatted_prompt)
         return result
