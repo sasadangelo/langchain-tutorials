099f9ad2283407da5c3bbc77f3cc4c96e1b3bd4d
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-22 13:00:49 +0200

    Add suammary memory strategy

--- lesson-8/src/memory/memory_factory.py
+++ lesson-8/src/memory/memory_factory.py
@@ -1,18 +1,23 @@
 from langchain.memory import (
     ConversationBufferMemory,
-    ConversationBufferWindowMemory
+    ConversationBufferWindowMemory,
+    ConversationSummaryMemory
 )
 from langchain.memory.chat_memory import BaseChatMemory
+from providers.provider_factory import LLMProviderFactory
 
 class MemoryFactory:
     @staticmethod
     def get_memory(config) -> BaseChatMemory:
         memory_type = config.get('chat_history_memory', 'buffer')
 
-        if memory_type == 'window':
+        if memory_type == 'buffer':
+            return ConversationBufferMemory(return_messages=True)
+        elif memory_type == 'window':
             window_size = config.get('chat_history_memory_window', 5)
             return ConversationBufferWindowMemory(k=window_size, return_messages=True)
-        elif memory_type == 'buffer':
-            return ConversationBufferMemory(return_messages=True)
+        elif memory_type == 'summary':
+            provider = LLMProviderFactory.get_provider(config)
+            return ConversationSummaryMemory(llm=provider.model, return_messages=True)
         else:
             raise ValueError(f"Unknown memory type: {memory_type}")
--- lesson-8/src/providers/provider_factory.py
+++ lesson-8/src/providers/provider_factory.py
@@ -11,10 +11,19 @@ class LLMProviderFactory:
         "watsonx": WatsonXProvider
     }
 
+    # The single provider instance
+    _instance = None
+
     @classmethod
     def get_provider(cls, config):
+        # If the instance already exists, return it
+        if cls._instance is not None:
+            return cls._instance
+
+        # Otherwise, create it
         provider_name = config['provider']
         provider_class = cls.providers.get(provider_name)
         if not provider_class:
             raise ValueError(f"Unsupported provider: {provider_name}")
-        return provider_class(config)
+        cls._instance = provider_class(config)
+        return cls._instance
