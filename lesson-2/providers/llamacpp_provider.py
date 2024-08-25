a2b8f812fdad0ed773982710db8510b802249ae3
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-26 00:13:44 +0200

    Fix llama cpp provider in the lesson 2

--- lesson-2/providers/llamacpp_provider.py
+++ lesson-2/providers/llamacpp_provider.py
@@ -1,14 +1,14 @@
 import os
 from providers.provider import LLMProvider
-from llama_cpp import Llama
+from langchain_community.llms import LlamaCpp
 
 class LLamaCppProvider(LLMProvider):
     def create_model(self):
         model_path = self.config['model_path']
         chat_format = self.config['chat_format']
         transformers_path = os.path.expanduser(self.config['transformers_path'])
-        self.model = Llama(model_path=transformers_path + "/" + model_path, chat_format=chat_format)
+        self.model = LlamaCpp(model_path=transformers_path + "/" + model_path, chat_format=chat_format)
 
     def generate(self, messages):
-        result = self.model.create_chat_completion(messages, max_tokens=None)
-        return result['choices'][0]['message']['content']
+        result = self.model.invoke(messages[0]['content'])
+        return result
