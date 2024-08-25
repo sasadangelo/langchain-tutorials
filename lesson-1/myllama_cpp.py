8a1b03ceaf96685ae315e3b99a85dfe7b9c1fc41
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-26 00:13:03 +0200

    Fix llama cpp provider in the lesson 1

--- lesson-1/myllama_cpp.py
+++ lesson-1/myllama_cpp.py
@@ -1,5 +1,5 @@
 import os
-from llama_cpp import Llama
+from langchain_community.llms import LlamaCpp
 
 # Define the local Hugging Faces cache directory
 cache_dir = os.getenv('TRANSFORMERS_CACHE', default=os.path.join(os.path.expanduser("~"), ".cache/huggingface/transformers"))
@@ -7,14 +7,7 @@ cache_dir = os.getenv('TRANSFORMERS_CACHE', default=os.path.join(os.path.expandu
 # Specify the model path
 model_path = cache_dir + "/llama-2-7b-chat-gguf/llama-2-7b-chat.Q2_K.gguf"
 
-llm = Llama(model_path=model_path, chat_format="llama-2")
+llm = LlamaCpp(model_path=model_path, chat_format="llama-2")
 
-messages = [
-    {"role": "user", "content": "Who is Robinson Crusoe?"},
-]
-
-# Execute the text generation
-result = llm.create_chat_completion(messages)
-
-# Print the result
-print(result['choices'][0]['message']['content'])
+output=llm.invoke("Who is Robinson Crusoe?")
+print(output)
