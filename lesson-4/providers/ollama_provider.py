718a09cc0469731fe0cc197458779729680683a8
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-08-27 19:01:27 +0200

    Minor fixes in lesson 4

--- lesson-4/providers/ollama_provider.py
+++ lesson-4/providers/ollama_provider.py
@@ -1,16 +1,10 @@
 from providers.provider import LLMProvider
 from langchain_community.llms import Ollama
-# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 class OllamaProvider(LLMProvider):
     def create_model(self):
         model_name = self.config['model']
         self.model = Ollama(model=model_name)
-        # self.chat_prompt_template = ChatPromptTemplate.from_messages(
-        #     [
-        #         MessagesPlaceholder(variable_name="messages"),
-        #     ]
-        # )
 
     def generate(self, prompt):
         print("****************************************************************")
