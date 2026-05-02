8b13ec7fcc3e965bcbd024125e1f698efa0d3d74
Salvatore D'Angelo <sasadangelo@gmail.com>
2026-05-02 20:32:37 +0200

    Add streaming to the lesson-7

--- lesson-7/src/protocols/ollama_protocol.py
+++ lesson-7/src/protocols/ollama_protocol.py
@@ -2,11 +2,12 @@
 # Copyright (c) 2026 Salvatore D'Angelo, Code4Projects
 # Licensed under the MIT License. See LICENSE.md for details.
 # -----------------------------------------------------------------------------
+from collections.abc import Iterator
 from typing import Any
 
 from core import LoggerManager, chatterpy_config
 from langchain_core.language_models.base import LanguageModelInput
-from langchain_core.messages import AIMessage
+from langchain_core.messages import AIMessage, AIMessageChunk
 from langchain_ollama import ChatOllama
 from protocols.protocol import LLMProtocol
 
@@ -54,3 +55,6 @@ class OllamaProtocol(LLMProtocol):
 
     def invoke(self, messages: LanguageModelInput) -> AIMessage:
         return self._protocol.invoke(input=messages)
+
+    def stream(self, messages: LanguageModelInput) -> Iterator[AIMessageChunk]:
+        return self._protocol.stream(input=messages)
